ea25d20d61a6a8516c09467f9b9fde2eb9c46d19
Esitaw <147694748+Esitaw@users.noreply.github.com>
2024-12-13 22:07:08 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<?php
+?>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> FindHire. All rights reserved.</p>
+    </footer>
+    <script src="assets/styles.css"></script> 
+</body>
+</html>
